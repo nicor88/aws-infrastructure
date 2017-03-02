61622195e30872b35d81e8c3d8c10b5750449189
nicor88 <ncorda@babbel.com>
2017-03-02 14:04:57 +0100

    cloudromation boto client

--- troposphere_templates/dev_server.py
+++ troposphere_templates/dev_server.py
@@ -1,10 +1,9 @@
-import os
 import boto3
 
 session = boto3.Session(profile_name='nicor88-aws-dev')
-s3_client = session.client('s3')
-
+cfn = session.client('cloudformation')
 
 # TODO create a security group
 # TODO create an EC2 machine
 
+STACK_NAME = 'DevServer'
