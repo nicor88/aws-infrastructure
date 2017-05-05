7c9d46ee7f06d52e937ca1048dd5b329092ced54
nicor88 <ncorda@babbel.com>
2017-05-05 21:44:11 +0200

    some simple staff

--- cloudformation/template_generator/dev_server.py
+++ cloudformation/template_generator/dev_server.py
@@ -3,7 +3,15 @@ import boto3
 session = boto3.Session(profile_name='nicor88-aws-dev')
 cfn = session.client('cloudformation')
 
+from troposphere import FindInMap, Parameter, Ref, Tags, Template
+
 # TODO create a security group
 # TODO create an EC2 machine
+# Deploy using code deploy
+# cfn-hup setup
 
 STACK_NAME = 'DevServer'
+
+template = Template()
+description = 'Dev Server Stack Free Tier'
+template.add_description(description)
