03b10a14569e0da93e158940a9f88776bdf977d5
nicor88 <ncorda@babbel.com>
2017-05-11 18:23:27 +0200

    refactor test functions

--- boto3_examples/kinesis.py
+++ boto3_examples/kinesis.py
@@ -3,6 +3,8 @@ import boto3
 import json
 import logging
 import boto3_examples.utils as u
+from pkg_resources import resource_string
+import ruamel_yaml as yaml
 
 # setup
 logger = logging.getLogger(__name__)
@@ -11,11 +13,12 @@ os.environ["AWS_PROFILE"] = "nicor88-aws-dev"
 
 # configure client using bot3
 kinesis = boto3.client('kinesis')
+cfg = yaml.load(resource_string('cloudformation.config', 'stream_config.yml'))
 
 
 # put a single record to the stream
-def put_one_record_to_kinesis():
-    res = kinesis.put_record(StreamName='test_stream',
+def put_one_record_to_kinesis(*, stream_name):
+    res = kinesis.put_record(StreamName=stream_name,
                              Data=json.dumps(
                                  u.create_kinesis_record(producer='python_script',
                                                          name='test:insert:one_record')),
@@ -25,7 +28,7 @@ def put_one_record_to_kinesis():
 
 
 # put multiple records in the stream
-def put_many_records_to_kinesis(records_number):
+def put_many_records_to_kinesis(*, stream_name, records_number):
     records = []
     for i in range(records_number):
         record = u.create_kinesis_record(producer='python_script',
@@ -33,18 +36,18 @@ def put_many_records_to_kinesis(records_number):
         records.append(record)
     k_records = u.prepare_kinesis_records(records=records, partition_key='created_at')
     logger.info('Putting multiple {} records'.format(len(k_records)))
-    res = kinesis.put_records(StreamName='test_stream', Records=k_records)
+    res = kinesis.put_records(StreamName=stream_name, Records=k_records)
     logger.info(res)
     return res
 
 
 # read records from the stream with TRIM_HORIZON
-def read_records_from_kinesis():
-    stream = kinesis.describe_stream(StreamName='test_stream')
+def read_records_from_kinesis(*, stream_name):
+    stream = kinesis.describe_stream(StreamName=stream_name)
     iterator = kinesis.get_shard_iterator(
         StreamName='test_stream',
         ShardId=stream['StreamDescription']['Shards'][0]['ShardId'],
-        ShardIteratorType='TRIM_HORIZON'
+        ShardIteratorType='LATEST'
     )
     records_from_the_stream = kinesis.get_records(ShardIterator=iterator['ShardIterator'],
                                                   Limit=200)['Records']
@@ -52,6 +55,8 @@ def read_records_from_kinesis():
 
     return records_from_the_stream
 
-# put_one_record_to_kinesis()
-# put_many_records_to_kinesis(5)
-# read_records_from_kinesis()
+# put_one_record_to_kinesis(stream_name='test_stream')
+# put_many_records_to_kinesis(stream_name='test_stream', records_number=5)
+# read_records_from_kinesis(stream_name='test_stream')
+
+# put_many_records_to_kinesis(stream_name=cfg['kinesis_stream_name'], records_number=5)
