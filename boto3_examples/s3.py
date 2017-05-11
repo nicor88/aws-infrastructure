0d035e6d3539b9373bf6ab5516db9998ba8c3e06
nicor88 <ncorda@babbel.com>
2017-05-11 08:24:56 +0200

    fix some error in s3 example

--- boto3_examples/s3.py
+++ boto3_examples/s3.py
@@ -1,7 +1,7 @@
 import boto3
 import os
 
-os.environ["AWS_DEFAULT_REGION"] = "us-east-1"
+os.environ["AWS_DEFAULT_REGION"] = "us-west-1"
 os.environ["AWS_PROFILE"] = "nicor88-aws-dev"
 
 s3 = boto3.client('s3')
@@ -11,13 +11,13 @@ s3 = boto3.client('s3')
 # s3 = session.client('s3')
 
 # get all the buckets
-buckets = s3.list_buckets()
+buckets = s3.list_buckets()['Buckets']
 
 # retrieve buckets names
 buckets_names = [p['Name'] for p in s3.list_buckets()['Buckets']]
 
 # retrieve bucket content
-objs = s3.list_objects_v2(Bucket=buckets[0])['Contents']
+objs = s3.list_objects_v2(Bucket=buckets[0]['Name'])['Contents']
 objs_keys = [o['Key'] for o in objs]
 
 # retrieve content of a bucket inside a folder
