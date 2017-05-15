e6d230540dc433a636dfd4e77d1e198744e7d858
nicor88 <ncorda@babbel.com>
2017-05-15 18:50:56 +0200

    added custom resources

--- cloudformation/template_generator/dev_lambdas.py
+++ cloudformation/template_generator/dev_lambdas.py
@@ -4,6 +4,7 @@ from pkg_resources import resource_string
 import ruamel_yaml as yaml
 
 from troposphere import awslambda, iam
+from troposphere.cloudformation import AWSCustomObject
 from awacs.aws import Statement, Allow, Deny, Policy, Action, Condition
 from troposphere import Template, Tags, Output, Ref, Parameter, GetAtt
 
@@ -74,15 +75,51 @@ hello_world_lambda = template.add_resource(
     )
 )
 
+
+# test to see if a lambda function can be triggered with a custom
+class CustomResource(AWSCustomObject):
+    resource_type = "Custom::CustomResourceTest"
+
+    props = {
+        'ServiceToken': (str, True)
+    }
+
+custom_resource_test_lambda = template.add_resource(
+    awslambda.Function(
+        'CustomResourceLambda',
+        FunctionName='custom_resource',
+        Description='Custom Resource Test lambda with Python 3.6',
+        Handler='lambda_function.lambda_handler',
+        Role=GetAtt('ExecutionRole', 'Arn'),
+        Code=awslambda.Code(
+            S3Bucket=cfg['s3_deployment_bucket'],
+            S3Key='deployments/lambdas/custom_resource.zip',
+        ),
+        Runtime='python3.6',
+        Timeout='30',
+        MemorySize=128
+    )
+)
+
+
+custom_resource = template.add_resource(
+    CustomResource('CustomResource',
+                   DependsOn='CustomResourceLambda',
+                   ServiceToken=GetAtt(custom_resource_test_lambda, 'Arn')
+                   )
+)
+
+
 template.add_output([
     Output('LambdaExecutionRole',
            Description='Lambdas Execution role',
            Value=Ref(lambda_execution_role))])
 
 template.add_output([
-    Output('HelloWorld',
-           Description='HelloWorld Lambda function',
-           Value=Ref(hello_world_lambda))])
+    Output('CustomResourcePurpose',
+           Description='Custom Purpose setup by the lambda function',
+           Value=GetAtt('CustomResource', 'Purpose')
+           )])
 
 template_json = template.to_json(indent=4)
 print(template_json)
