c7424f3d82ce2dd25f7dad2bf85b0f84f5ec4c0f
nicor88 <ncorda@babbel.com>
2017-07-24 22:32:56 +0200

    added implementation of bootstrap conda

---
+++ emr/bootstrap_actions/bootstrap_conda.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+
+# Install conda
+wget https://repo.continuum.io/miniconda/Miniconda3-4.2.12-Linux-x86_64.sh -O /home/hadoop/miniconda.sh \
+    && /bin/bash ~/miniconda.sh -b -p $HOME/conda
+
+echo '\nexport PATH=$HOME/conda/bin:$PATH' >> $HOME/.bashrc && source $HOME/.bashrc
+
+conda config --set always_yes yes --set changeps1 no
+
+conda install conda=4.2.13
+
+conda config -f --add channels conda-forge
+conda config -f --add channels defaults
+
+conda install hdfs3 findspark ujson jsonschema toolz boto3 py4j numpy pandas==0.19.2
