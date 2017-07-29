b3905418830bdce26a27d969535037499cdc43ce
nicor88 <ncorda@babbel.com>
2017-07-30 00:16:13 +0200

    changed the json to parquet

--- emr/steps/example_save_as_parquet.py
+++ emr/steps/example_save_as_parquet.py
@@ -4,9 +4,12 @@
 """
 
 import boto3
+import datetime as dt
+import hashlib
 import logging
 from pyspark.sql import SparkSession
 from pyspark.sql import SQLContext
+
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
@@ -22,6 +25,13 @@ df = sqlContext.read.json('s3://nicor-data/samples/world_bank.json')
 # TODO send events to cloudwatch
 
 # save df as parquet
-df.repartion('1').write.parquet('s3://nicor-data/test-write/')
+now_str = str(dt.datetime.now())
+now_hash = hashlib.md5(now_str.encode('utf-8')).hexdigest()
+bucket_name = 'nicor-data'
+base_bucket = 's3://{}'.format(bucket_name)
+
+destination_path = '{}/parquet/test-write-{}'.format(base_bucket, now_hash)
+# df.repartition(2).write.parquet(destination_path)
+df.write.parquet(destination_path)
 
 spark.stop()
