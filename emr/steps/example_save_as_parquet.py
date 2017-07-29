c0c3f8152f554ed97cd14448e548eb5097ae7237
nicor88 <ncorda@babbel.com>
2017-07-29 20:43:58 +0200

    added some example and todo

--- emr/steps/example_save_as_parquet.py
+++ emr/steps/example_save_as_parquet.py
@@ -1,3 +1,9 @@
+"""
+# how to call in the master node of the cluster
+# spark-submit --deploy-mode client --master yarn example_save_as_parquet.py
+"""
+
+import boto3
 import logging
 from pyspark.sql import SparkSession
 from pyspark.sql import SQLContext
@@ -12,6 +18,8 @@ sqlContext = SQLContext(sc)
 
 # read
 df = sqlContext.read.json('s3://nicor-data/samples/world_bank.json')
+
+# TODO send events to cloudwatch
 
 # save df as parquet
 df.repartion('1').write.parquet('s3://nicor-data/test-write/')
