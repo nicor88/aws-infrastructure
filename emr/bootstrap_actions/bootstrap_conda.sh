fb328614cb29ba79a596ed3ce864debf9f8e6a1e
nicor88 <ncorda@babbel.com>
2017-07-29 12:55:08 +0200

    changed bootstrap script

--- emr/bootstrap_actions/bootstrap_conda.sh
+++ emr/bootstrap_actions/bootstrap_conda.sh
@@ -4,9 +4,21 @@
 wget https://repo.continuum.io/miniconda/Miniconda3-4.3.21-Linux-x86_64.sh -O /home/hadoop/miniconda.sh
 /bin/bash /home/hadoop/miniconda.sh -b -p /home/hadoop/miniconda
 
+# for debug
+echo $USER
+echo $HOME
+
+
+# setup conda
+echo -e "\nexport PATH=/home/hadoop/conda/bin:$PATH" >> /home/hadoop/.bashrc
+source /home/hadoop/.bashrc
+
 conda config --set always_yes yes --set changeps1 no
 conda config -f --add channels conda-forge
 conda config -f --add channels defaults
 
 # install libs
 conda install hdfs3 findspark ujson jsonschema toolz boto3 py4j numpy pandas
+
+# cleanup
+rm ~/miniconda.sh
