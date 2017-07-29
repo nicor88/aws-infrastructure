c2d66afc5eabb6b640d444ab6ce0556f49c95c5a
nicor88 <ncorda@babbel.com>
2017-07-30 00:15:52 +0200

    simple example in boto3 for emr to create a step using the utils

--- boto3_examples/add_emr_steps.py
+++ boto3_examples/add_emr_steps.py
@@ -1,32 +1,7 @@
-import boto3
-
-emr = boto3.client('emr')
-
-
-def spark_submit(*, cluster_id, args, step_name):
-    # description cluster
-    desc = emr.describe_cluster(ClusterId=cluster_id)
-    print('Adding step to', cluster_id, ':', desc.get('Cluster', {}).get('Name'))
-
-    all_args = ['spark-submit',
-                '--deploy-mode',
-                'client',
-                '--master',
-                'yarn'
-                ]
-    all_args.extend(args)
-
-    step_example_with_params = {'ActionOnFailure': 'CONTINUE',
-                                'HadoopJarStep': {'Args': all_args,
-                                                  'Jar': 'command-runner.jar'},
-                                'Name': step_name}
-    print(step_example_with_params)
-
-    response = emr.add_job_flow_steps(JobFlowId=cluster_id,
-                                      Steps=[step_example_with_params])
-    return response
+import emr.utils.steps as steps
+import cloudformation.utils as utils
 
 # example
-# cluster_id = 'your_cluster_id'
-# args = ['/home/hadoop/tasks/to_execute.py', 'arg1', 'arg2']
-# spark_submit(cluster_id=cluster_id, step_name='Example Step', args=args)
+cluster_id = utils.get_cluster_id(stack_name='GenericEMRStack')
+args = ['/home/hadoop/steps/example_save_as_parquet.py', 'arg1']
+steps.spark_submit(cluster_id=cluster_id, step_name='JSON to Parquet', args=args)
