15409d0540aefb194c68af0b06a82588ae5dd46b
nicor88 <ncorda@babbel.com>
2017-07-29 20:44:06 +0200

    utils to get stack_id

--- cloudformation/utils.py
+++ cloudformation/utils.py
@@ -1,5 +1,6 @@
 import cfn_flip
 import logging
+import boto3
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
@@ -13,3 +14,14 @@ def write_template(**stack_args):
     with open(cfn_yaml_path, 'wt') as f:
         f.write(cfn_flip.to_yaml(stack_args['TemplateBody']))
         logger.info('wrote yaml template')
+
+
+def get_cluster_id(*,stack_name, ClusterResourceName='Cluster'):
+    cfn = boto3.client('cloudformation')
+    try:
+        cluster = cfn.describe_stack_resource(StackName=stack_name,
+                                             LogicalResourceId=ClusterResourceName)['StackResourceDetail']
+        cluster_id = cluster['PhysicalResourceId']
+        return cluster_id
+    except Exception as e:
+        raise e
