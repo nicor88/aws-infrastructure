5d09e7416353d8520de98c69974e26d64af3705d
nicor88 <ncorda@babbel.com>
2017-07-30 00:24:40 +0200

    example ho to delete the stack (containing the cluster)

--- boto3_examples/add_emr_steps.py
+++ boto3_examples/add_emr_steps.py
@@ -5,3 +5,8 @@ import cloudformation.utils as utils
 cluster_id = utils.get_cluster_id(stack_name='GenericEMRStack')
 args = ['/home/hadoop/steps/example_save_as_parquet.py', 'arg1']
 steps.spark_submit(cluster_id=cluster_id, step_name='JSON to Parquet', args=args)
+
+# example to delete a stack from emr
+cluster_id = utils.get_cluster_id(stack_name='GenericEMRStack')
+args = ['python', '/home/hadoop/steps/delete_stack.py', '--stack-name', 'GenericEMRStack']
+steps.send_command(cluster_id=cluster_id, step_name='Delete CFN stack', args=args)
