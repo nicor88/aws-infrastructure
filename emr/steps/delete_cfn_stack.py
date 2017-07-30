6dd767c2dc170303558e28505de4d9c2407c6391
nicor88 <ncorda@babbel.com>
2017-07-30 14:23:21 +0200

    increased timeout and and add read to payload

--- emr/steps/delete_cfn_stack.py
+++ emr/steps/delete_cfn_stack.py
@@ -23,12 +23,12 @@ def create_argparser():
 if __name__ == "__main__":
     args = create_argparser().parse_args()
     stack_name = vars(args).get('stack_name')
-    sleep_time = 10
+    sleep_time = 30
     logger.info('Deleting Stack {} in {} seconds'.format(stack_name,sleep_time))
     time.sleep(sleep_time)
     aws_lambda = boto3.client('lambda')
     function_name = 'delete_cfn_stack'
     res = aws_lambda.invoke(FunctionName=function_name,
                             Payload=json.dumps({'stack_name': stack_name}))
-    logger.info(res['Payload'].decode())
+    logger.info(res['Payload'].read().decode())
 
