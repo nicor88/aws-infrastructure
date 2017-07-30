c66aaa6e327806c259087b72c0989fc0bdf327ba
nicor88 <ncorda@babbel.com>
2017-07-30 13:31:14 +0200

    added a delay on step deleting

--- boto3_examples/add_emr_steps.py
+++ boto3_examples/add_emr_steps.py
@@ -7,7 +7,9 @@ cluster_id = utils.get_cluster_id(stack_name='GenericEMRStack')
 args = ['/home/hadoop/steps/example_save_as_parquet.py']
 steps.spark_submit(cluster_id=cluster_id, step_name='JSON to Parquet', args=args)
 
+
+# TODO before deleting the stack put another step in between doing nothing
 # example to delete a stack from emr
 # cluster_id = utils.get_cluster_id(stack_name='GenericEMRStack')
-# args = ['/home/hadoop/miniconda/bin/python', '/home/hadoop/steps/deleting_cfn_stack.py', '--stack-name', 'GenericEMRStack']
+# args = ['/home/hadoop/miniconda/bin/python', '/home/hadoop/steps/delete_cfn_stack.py', '--stack-name', 'GenericEMRStack']
 # steps.send_command(cluster_id=cluster_id, step_name='Delete CFN stack', args=args)
--- emr/steps/delete_cfn_stack.py
+++ emr/steps/delete_cfn_stack.py
@@ -1,6 +1,7 @@
 import boto3
 import argparse
 import logging
+import time
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
@@ -21,7 +22,7 @@ def create_argparser():
 if __name__ == "__main__":
     args = create_argparser().parse_args()
     stack_name = vars(args).get('stack_name')
-
-    logger.info('Deleting Stack: {}'.format(stack_name))
+    logger.info('Deleting Stack {} in 60 seconds'.format(stack_name))
+    time.sleep(60)
     cfn = boto3.client('cloudformation')
     cfn.delete_stack(StackName=stack_name)
