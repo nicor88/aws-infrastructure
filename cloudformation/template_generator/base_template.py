55e924bc19ab4b84c513c33591cb73a22ac4a314
nicor88 <ncorda@babbel.com>
2017-07-30 23:27:21 +0200

    added a base template to init other templates

---
+++ cloudformation/template_generator/base_template.py
@@ -0,0 +1,53 @@
+import boto3
+from pkg_resources import resource_string
+import ruamel_yaml as yaml
+
+from troposphere import GetAtt, Output, Parameter, Ref, Tags, Template
+
+import cloudformation.utils as utils
+
+# load config
+# cfg = yaml.load(resource_string('cloudformation.config', 'base_config.yml'))
+
+STACK_NAME = 'BaseStack'
+
+template = Template()
+description = 'Description for the stack'
+template.add_description(description)
+template.add_version('2010-09-09')
+
+resource = template.add_resource(
+    # TODO add some resource
+)
+
+# Outputs
+template.add_output([
+    Output('MyOutput',
+           Description='Reference to the resource',
+           Value=Ref(resource)),
+    Output('MyOutputAtt',
+           Description='Reference to the resource attibute',
+           Value=GetAtt(resource, 'Resource attribute')),
+])
+
+template_json = template.to_json(indent=4)
+print(template_json)
+
+stack_args = {
+    'StackName': STACK_NAME,
+    'TemplateBody': template_json,
+    'Tags': [
+        {
+            'Key': 'Purpose',
+            'Value': 'BaseTemplate'
+        }
+    ]
+}
+
+cfn = boto3.client('cloudformation')
+cfn.validate_template(TemplateBody=template_json)
+# utils.write_template(**stack_args)
+
+# cfn.create_stack(**stack_args)
+# cfn.update_stack(**stack_args)
+# cfn.delete_stack(StackName=STACK_NAME)
