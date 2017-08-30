41b612507598536136c7d361aeaece9c76e37a58
nicor88 <ncorda@babbel.com>
2017-08-30 11:07:23 +0200

    some clean up and put examples on top

--- emr/steps/streaming.py
+++ emr/steps/streaming.py
@@ -1,50 +1,58 @@
-from __future__ import print_function
+"""
+Sample spark job to process events from a Kinesis Stream
+
+# Example 1: submit the spark job using only the master with 4 cores
+spark-submit --deploy-mode client --packages org.apache.spark:spark-streaming-kinesis-asl_2.11:2.2.0 --master local[4] steps/streaming.py
+
+# Example 2: submit the spark job using yarn with specified amount of executors
+spark-submit --deploy-mode client --packages org.apache.spark:spark-streaming-kinesis-asl_2.11:2.2.0 --master yarn --num-executors 4 steps/streaming.py
+
+# Notes
+use a number of executors multiple of the number of shards (one executor for core at least)
+
+# References
+https://github.com/MayankAyush/KinesisSparkIntegration/blob/41707745c477b8e39c724b7ffd167bd4ff690885/pysparkKinesisIntegration.py
+https://github.com/apache/spark/blob/master/external/kinesis-asl/src/main/python/examples/streaming/kinesis_wordcount_asl.py
+
+"""
+import json
+
 from pyspark.sql import SparkSession
+from pyspark.sql import SQLContext
 from pyspark.streaming import StreamingContext
 from pyspark import StorageLevel
 from pyspark.streaming.kinesis import KinesisUtils, InitialPositionInStream
-import json
 
-
-spark = SparkSession.builder.appName('test').getOrCreate()
-sc = spark.sparkContext
-streaming_ctx = StreamingContext(sc, 10)
-
-
-def printRecord(rdd):
-    print("========================================================")
-    print("Starting new RDD")
-    print("========================================================")
-    # rdd.foreach(lambda record: print(record.encode('utf8')))
-    print('---------> Cound of RDD {}'.format(rdd.count()))
-    # TODO to save use append mode
-    # rdd.saveAsTextFile('s3://nicor-data/test-streaming/')
-
-app_name = 'TestStreaming'
-stream_name = 'DevStreamS3'
+app_name = 'test_spark_streaming'
+kinesis_stream_name = 'DevStreamS3'
 region_name = 'eu-west-1'
-endpoint_url = 'https://kinesis.eu-west-1.amazonaws.com'
+kinesis_endpoint_url = 'https://kinesis.eu-west-1.amazonaws.com'
+windows_size_secs = 10
+
+spark = SparkSession.builder.appName(app_name).getOrCreate()
+sc = spark.sparkContext
+sql_ctx = SQLContext(sc)
+streaming_ctx = StreamingContext(sc, windows_size_secs)
+
+
+def handle_rdd(rdd):
+    print("---------> Processing new RDD")
+    # rdd.foreach(lambda record: print(record.encode('utf8')))
+    rdd_transformed = rdd.map(lambda e: json.loads(e))
+    df = rdd_transformed.toDF()
+    print('---------> Count of RDD {}'.format(rdd_transformed.count()))
+    print('---------> Count of DF {}'.format(df.count()))
+
 
 dstream = KinesisUtils.createStream(streaming_ctx,
-                                    app_name, stream_name,
-                                    endpoint_url,
+                                    app_name, kinesis_stream_name,
+                                    kinesis_endpoint_url,
                                     region_name,
                                     InitialPositionInStream.LATEST,
-                                    10,
+                                    windows_size_secs,
                                     StorageLevel.MEMORY_AND_DISK_2)
-dstream.foreachRDD(printRecord)
+dstream.foreachRDD(handle_rdd)
 streaming_ctx.start()
 streaming_ctx.awaitTermination()
 
-# examples
-# spark-submit --deploy-mode client --packages org.apache.spark:spark-streaming-kinesis-asl_2.11:2.2.0 --master yarn steps/streaming.py
-# https://github.com/MayankAyush/KinesisSparkIntegration/blob/41707745c477b8e39c724b7ffd167bd4ff690885/pysparkKinesisIntegration.py
-# https://github.com/apache/spark/blob/master/external/kinesis-asl/src/main/python/examples/streaming/kinesis_wordcount_asl.py
-
-# submit job example
-# spark-submit --deploy-mode client --packages org.apache.spark:spark-streaming-kinesis-asl_2.11:2.2.0 --master local[4] steps/streaming.py
-# spark-submit --deploy-mode client --packages org.apache.spark:spark-streaming-kinesis-asl_2.11:2.2.0 --master yarn --num-executors 1 steps/streaming.py
-
-## Some learnings
-# use a number of executors multiple of the number of shards
-# one executor for core at least
+# streaming_ctx.stop()
