dbdb10884dff23c0e16f84cdca73aab1bc74f9dd
nicor88 <ncorda@babbel.com>
2017-08-30 11:56:20 +0200

    fix streaming app

--- emr/steps/streaming.py
+++ emr/steps/streaming.py
@@ -24,7 +24,7 @@ from pyspark import StorageLevel
 from pyspark.streaming.kinesis import KinesisUtils, InitialPositionInStream
 
 app_name = 'test_spark_streaming'
-kinesis_stream_name = 'DevStreamS3'
+kinesis_stream_name = 'DevStream'
 region_name = 'eu-west-1'
 kinesis_endpoint_url = 'https://kinesis.eu-west-1.amazonaws.com'
 windows_size_secs = 10
@@ -35,13 +35,19 @@ sql_ctx = SQLContext(sc)
 streaming_ctx = StreamingContext(sc, windows_size_secs)
 
 
+def process_event(event):
+    json_event = json.loads(event)
+    print(json_event)
+    return json_event
+
+
 def handle_rdd(rdd):
     print("---------> Processing new RDD")
-    # rdd.foreach(lambda record: print(record.encode('utf8')))
-    rdd_transformed = rdd.map(lambda e: json.loads(e))
-    df = rdd_transformed.toDF()
-    print('---------> Count of RDD {}'.format(rdd_transformed.count()))
-    print('---------> Count of DF {}'.format(df.count()))
+    rdd_count = rdd.count()
+    print('---------> Count of Initial RDD {}'.format(rdd_count))
+    if rdd_count > 0:
+        rdd_transformed = rdd.map(lambda e: process_event(e))
+        print('---------> Count of Transformed RDD {}'.format(rdd_transformed.count()))
 
 
 dstream = KinesisUtils.createStream(streaming_ctx,
